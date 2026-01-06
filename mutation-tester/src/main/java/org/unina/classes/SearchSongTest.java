27c2b954f185c5cffb27bc20daf6f2ea0351b144
sim-liberti <sim.liberti+github@gmail.com>
2026-01-06 10:53:16 +0100

    [misc] added a test class

---
+++ mutation-tester/src/main/java/org/unina/classes/SearchSongTest.java
@@ -0,0 +1,55 @@
+package org.unina.classes;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class SearchSongTest extends BaseTest {
+    @Test
+    public void testSearchSong() throws Exception {
+        driver.get(baseUrl);
+        driver.findElement(By.linkText("Search")).click();
+        driver.findElement(By.xpath("//input")).clear();
+        driver.findElement(By.xpath("//input")).sendKeys("Michael Jackson");
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Title'])[1]/following::*[name()='svg'][2]")).click();
+        driver.findElement(By.linkText("Home")).click();
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
